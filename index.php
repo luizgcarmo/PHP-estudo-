d9ab29dc083f66693335a8926bcbf81ed0278753
Luiz Gustavo <118779018+luizgcarmo@users.noreply.github.com>
2023-04-18 00:13:24 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <link rel="stylessheets" href="assets/styles.css"/>
+        
+        <title>Ola Mundo</title>
+    </head>
+    <body>
+        <?php
+        /**
+        *   Bloco de comentarios com variavel start
+        *   e comando de saida.
+        */
+        $start = "Vamos Começar!";
+        echo "<h2>Ola Mundo ! $start</h2>";
+        echo "<p id='js'> Loading.... </p>";
+        ?>
+        <script src="assets/script.js"></script>
+    </body>
+</html>
